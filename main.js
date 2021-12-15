d20f034c42b472751053109f730cf395f410fb1d
ChomolungmaShirayuki <85323087+ChomolungmaShirayuki@users.noreply.github.com>
2021-12-15 14:03:04 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function footer(){
+    var html = "";
+    html += '<h1 class="logo"><img src="image/common/img_hz_main.png" style="height:60px;"></h1>';
+    html += ' <p id="head"><img src="image/common/head.jpeg" class="headimg"></p>';
+    html += '<p style="height: 8px;background-image: repeating-linear-gradient(45deg, #ccc 0, #ccc 1px, transparent 0, transparent 50%);background-size: 8px 8px;"></p>';
+    document.write(html);
+    }
