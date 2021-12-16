d569d34acbef2b5d8ff5d429b1befa91261f303d
ChomolungmaShirayuki <85323087+ChomolungmaShirayuki@users.noreply.github.com>
2021-12-16 09:37:20 +0900

    Add files via upload

--- mine/load.js
+++ mine/load.js
@@ -1,3 +1,19 @@
 $(function() {
-    $('#footer').load('html/footer.html');
+    $('#load_footer').load('html/footer.html');
+   });
+
+$(function() {
+    $('#load_header').load('html/header.html');
+   });
+
+$(function() {
+    $('#load_fs_menu').load('html/fs_menu.html');
+   });
+
+$(function() {
+    $('#load_sidemenu').load('html/sidemenu.html');
+   });
+
+$(function() {
+    $('#load_signature').load('html/signature.html');
    });
