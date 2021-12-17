cc51d1a5a7c413cd9345db98a9e03a9426d5160f
ChomolungmaShirayuki <85323087+ChomolungmaShirayuki@users.noreply.github.com>
2021-12-17 09:31:06 +0900

    Add files via upload

---
+++ load.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('#load_header').load('html/header.html');
+   });
+
+$(function() {
+    $('#load_sidemenu').load('html/sidemenu.html');
+   });
+
+$(function() {
+    $('#load_signature').load('html/signature.html');
+   });
+
+$(function() {
+    $('#load_footer').load('html/footer.html');
+   });
+   
+$(function() {
+    $('#load_fs_menu').load('html/fs_menu.html');
+   });
